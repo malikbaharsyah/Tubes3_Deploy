becdd18c88d07fedab5460c1f96ff0e715bf8a74
jaulthfh <13521030@std.stei.itb.ac.id>
2023-05-04 17:12:04 +0700

    update frontend 1.0

--- src/services/frontend/gui/pages/index.js
+++ src/services/frontend/gui/pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { LeftSideBar } from '../styles/Home.module.css';
+import leftSideBarStyles from '../styles/LeftSideBar.module.css';
+import ChatBoxStyles from '../styles/ChatBox.module.css';
+import RadioButtonStyles from '../styles/RadioButton.module.css';
+
+import { TextBox } from './textBox';
+import { RadioButton } from './radioButton';
+import { NewChat } from './newChat';
+ 
 
 export default function Home() {
   return (
@@ -17,9 +24,26 @@ export default function Home() {
         `}</style>
       </Head>
       <div className={`${styles.column} ${styles['home']}`}>
-        <div className={styles['home-atas']}>
+        <RadioButton />
+        <div className={`${leftSideBarStyles['left-sidebar']}`}>
+          <div className={`${leftSideBarStyles['sidebar-text']}`}>
+            <h2>HISTORY</h2>
+          </div>
+            <NewChat />
         </div>
+        <div className={`${leftSideBarStyles['bottom-left-sidebar']}`}>
+        </div>
+          <div className={`${ChatBoxStyles['chat-box']}`}>
+            <TextBox />
+          </div>
       </div>
+
+
+      <script>
+        {`
+          // Your JavaScript code here
+          `}
+      </script>
     </div>
   );
 }
---
+++ src/services/frontend/gui/pages/radioButton.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import styles from '../styles/RadioButton.module.css';
+
+export const RadioButton = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    const value = event.target.value;
+    if (value === selectedOption) {
+      setSelectedOption('');
+    } else {
+      setSelectedOption(value);
+    }
+  };
+
+  return (
+    <div>
+      <label className={styles.container}>
+        <input type="radio" name="option" value="option1" checked={selectedOption === 'option1'} onChange={handleOptionChange} />
+        <span className={styles.checkmark}></span>
+        Knuth Morris Pratt
+      </label>
+      <label className={styles.container}>
+        <input type="radio" name="option" value="option2" checked={selectedOption === 'option2'} onChange={handleOptionChange} />
+        <span className={styles.checkmark}></span>
+        Boyer Moore
+      </label>
+    </div>
+  );
+};
